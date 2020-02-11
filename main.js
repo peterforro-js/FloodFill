ef3adb3cc3f2d2a67b104d54e7206bd0a59ee444
Peter Forro <forropcs@gmail.com>
2020-02-11 19:52:29 +0100

    drawing feature, transition duration and delay is solved, cell numbering, draw  checkbox

--- main.js
+++ main.js
@@ -1,20 +1,58 @@
-function main(){
-    for(let i = 0; i < 100; ++i){
-        var div = document.createElement("div");
-        div.classList.add("cells");
-        document.querySelector("#track").appendChild(div);
+function initGoButtonClickEvent(){
+    document.querySelector("#go").addEventListener("click", _=>{
+        let color = document.querySelector("#color").value;
+        changeColor(color);
+    });
+}
+
+function changeColor(color){
+    let cells = Array.from(document.querySelectorAll(".cells"));
+    let cnt = 0;
+    cells.forEach(cell =>{
+        cell.style.transitionDelay = cnt++ * 10 + "ms";
+        cell.style.backgroundColor = color; 
+    });
+}
+
+function initTrackMouseDownEvent(){
+    let track = document.querySelector("#track");
+    track.addEventListener("mousedown", _=> {
+        track.setAttribute("data-draw", "1");
+    });
+}
+
+function initTrackMouseUpEvent(){
+    let track = document.querySelector("#track");
+    track.addEventListener("mouseup", _=> {
+        track.setAttribute("data-draw", "0");
+    });
+}
+
+function createCells(){
+    let color = document.querySelector("#color").value;
+    let track = document.querySelector("#track");
+    for(let y = 0; y < 50; ++y){
+        for(let x = 0; x < 50; ++x){
+            var div = document.createElement("div");
+            div.classList.add("cells");
+            div.dataset.y = y + 1;
+            div.dataset.x = x + 1;
+            div.addEventListener("mouseover", event => {
+            let draw = Boolean(Number(document.querySelector("#track").dataset.draw));
+            if (draw){
+                event.target.style.backgroundColor = color;
+            }
+        });
+        track.appendChild(div);
+        }
     }
 }
 
-function changeColor(){
-    let cells = Array.from(document.querySelectorAll(".cells"));
-    let transitionDelay = 0;
-    cells.forEach(cell =>{
-        cell.style.transitionDelay = transitionDelay + "ms";
-        cell.style.backgroundColor = cell.style.backgroundColor === "navy" ? "green" : "navy";
-        transitionDelay += 100;
-    });
-
+function main(){
+    createCells();
+    initTrackMouseDownEvent();
+    initTrackMouseUpEvent();
+    initGoButtonClickEvent();
 }
 
 main();
